b7c149d3a701ada265e29f1ed0c6f60a6b7c32c3
RhysWeb <Rhys25780@hotmail.com>
2021-12-30 23:18:30 +0000

    made a change

--- pages/index.js
+++ pages/index.js
@@ -26,8 +26,8 @@ export default function Home({ allPostsData }) {
 					I'm working on this next JSproject
 				</p>
 				<p>
-					(This is a sample website - you’ll be building a site like this on{' '}
-					<a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+					(BLah blah blah sample website - you’ll be building a site like this
+					on <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
 				</p>
 			</section>
 			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
